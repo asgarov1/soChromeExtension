da67c80c4b65718cae2a8aa737650af4db7bc9a8
Javid Asgarov <asgarov1@gmail.com>
2024-07-13 22:03:26 +0200

    added software recommendation answer

--- popup.js
+++ popup.js
@@ -5,6 +5,8 @@ const answers = [
     { id: 'You_Should_Accept_Answer', text: 'If one of the answers below fixes your issue, you should accept it (click the check mark next to the appropriate answer). That does two things. It lets everyone know your issue has been resolved to your satisfaction, and it gives the person that helps you credit for the assist. [See here](http://meta.stackexchange.com/a/5235) for a full explanation.'},
     { id: 'Bad_Formatting', text: 'Please consider revising the code sample you posted in this question. As it currently stands, its formatting and scope make it hard for us to help you; here is a [great resource](http://stackoverflow.com/help/mcve) to get you started on that. -1, don\'t take it the wrong way. A down vote is how we indicate a content problem around here; improve your formatting and code sample and I (or someone will) gladly revert it. Good luck with your code!'},
     { id: 'No_Images_Of_Code', text: 'Please include code, not images of code: see [Why not upload images of code errors when asking a question](https://meta.stackoverflow.com/questions/285551/why-should-i-not-upload-images-of-code-data-errors). Edit the question and include code fragments.'},
+    { id: 'set_debug_level', text: 'Please set logging level to debug with `logging.level.org.springframework.security=DEBUG` so that you see the error. After that paste the error, correctly formatted, into question'},
+    { id: 'software_recommendations', text: 'Off-topic here. Sister site: [softwarerecs.stackexchange.com](https://softwarerecs.stackexchange.com/)'},
 ]
 
 document.addEventListener(DOM_CONTENT_LOADED, function () {
